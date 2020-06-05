b55225f586e29636fdfdebe3cd8ee4a8d9d694ac
Hien Nguyen <toilati123vn@gmail.com>
2020-06-05 17:29:10 +0700

    [Feature] (Swagger): Init

--- app.api/Startup.cs
+++ app.api/Startup.cs
@@ -10,7 +10,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-[assembly:ApiController]
+using Microsoft.OpenApi.Models;
+
+[assembly: ApiController]
+
 namespace app.api
 {
     public class Startup
@@ -26,11 +29,14 @@ namespace app.api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "API", Version = "v1"}); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1"); });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -42,10 +48,7 @@ namespace app.api
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
