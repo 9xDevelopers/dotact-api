7c592523841cfa8e357f089ef8cdbc814f27e080
Hien Nguyen <toilati123vn@gmail.com>
2020-06-27 23:26:55 +0700

    [Refactor, Migration] (Database): Change identity table name from Plural to SIngular

--- app.infrastructure/Models/AppDbContext.cs
+++ app.infrastructure/Models/AppDbContext.cs
@@ -1,11 +1,12 @@
 using System;
 using app.core.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace app.infrastructure.Models
 {
-    public class AppDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+    public class AppDbContext : IdentityDbContext<AppUser,AppRole,Guid>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
@@ -24,6 +25,15 @@ namespace app.infrastructure.Models
                 modelBuilder.Entity(entityType.ClrType).ToTable(entityType.ClrType.Name);
             }
             base.OnModelCreating(modelBuilder);
+            
+            // Change Identity table from Plural to Singular
+            modelBuilder.Entity<AppUser>().ToTable("User");
+            modelBuilder.Entity<AppRole>().ToTable("Role");
+            modelBuilder.Entity<IdentityUserClaim<Guid>>().ToTable("UserClaim");
+            modelBuilder.Entity<IdentityUserRole<Guid>>().ToTable("UserRole");
+            modelBuilder.Entity<IdentityUserLogin<Guid>>().ToTable("UserLogin");
+            modelBuilder.Entity<IdentityRoleClaim<Guid>>().ToTable("RoleClaim");
+            modelBuilder.Entity<IdentityUserToken<Guid>>().ToTable("UserToken");
             //Write Fluent API configurations here  
         }
     }
