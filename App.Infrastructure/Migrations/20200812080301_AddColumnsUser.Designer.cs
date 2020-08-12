da792b66eb29c7a0dc77dbcd1a5b65a43dcbca9c
Truc Phan <anhtrucphanit@gmail.com>
2020-08-12 15:06:13 +0700

    Add 2 columns in User table

--- App.Infrastructure/Migrations/20200812080301_AddColumnsUser.Designer.cs
+++ App.Infrastructure/Migrations/20200812080301_AddColumnsUser.Designer.cs
@@ -4,14 +4,16 @@ using App.Infrastructure.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace App.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200812080301_AddColumnsUser")]
+    partial class AddColumnsUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
